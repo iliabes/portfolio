a39c9f95b366c50a010ee7ebdc4f9887db5a4646
Forgetfull <oksana14888@gmail.com>
2021-09-02 02:43:04 +0500

    add animte.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import './script/preload'
 import './script/alert'
 import './script/swipe'
+import './script/animate' 
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
