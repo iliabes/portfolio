7bd34ff13b4d8663daa455c9ef52a607de603e07
f0rGetFull <f0rgetful@yandex.ru>
2021-09-03 00:48:33 +0300

    animate via data-attr & elem interval

--- src/script/animate.js
+++ src/script/animate.js
@@ -1,3 +1,26 @@
 export function catchData(elem){
-    console.log(elem.querySelectorAll('.yo'))
+    let elems = elem.querySelectorAll('[data-yo]')
+    // elems.forEach(element => {
+    //     console.log(element)
+    //     // element.animate(animation, ooptionAnimation)
+    //     element.style.animation = "blah 3s ease-in-out "
+    
+    // });
+    for (let i = 0; i < elems.length; i++) {
+        (function(i) {
+            setTimeout(function() {
+                elems[i].style.animation = "blah 1s ease-in-out forwards"
+            }, 500 * i);
+        })(i);
+    }
+
+}
+
+let animation = [
+    { transform: 'translateX(-1000px)' },
+    { transform: 'translateX(0)'  } 
+]
+
+let ooptionAnimation = {
+    duration: 2000
 }
